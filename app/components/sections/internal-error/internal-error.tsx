05bd53f5978c964ac1796ef68b6867d4ca4e572e
Adi Primanda Ginting <adi@adipginting.com>
2025-09-16 06:17:51 +0700

    feat: redirect to home when clicked on button on not found page

--- app/components/sections/internal-error/internal-error.tsx
+++ app/components/sections/internal-error/internal-error.tsx
@@ -1,22 +1,17 @@
 import type { FC } from "react";
 
 export const InternalErrorSection: FC = () => (
-	<section className="h-screen flex flex-col items-center">
-		<div className="mt-[19vh]">
-			<img
-				src="/images/logo-dark.svg"
-				className="w-73 h-25"
-				alt="pycon 25 logo"
-			></img>
-		</div>
+  <section className="h-screen flex flex-col items-center">
+    <div className="mt-[19vh]">
+      <img
+        src="/images/logo-dark.svg"
+        className="w-73 h-25"
+        alt="pycon 25 logo"
+      ></img>
+    </div>
 
-		<div className="mt-[46vh]">
-			<button
-				type="button"
-				className="cursor-pointer bg-[#224083] w-77 h-16 font-sans rounded-xl text-[#FAFAFA] text-2xl"
-			>
-				Back to Homepage
-			</button>
-		</div>
-	</section>
+    <div className="font-sans text-[#282828] text-md mt-[46vh] text-center">
+      You are going to be redirected back to the homepage.
+    </div>
+  </section>
 );
--- app/components/sections/not-found/not-found.tsx
+++ app/components/sections/not-found/not-found.tsx
@@ -1,17 +1,29 @@
 import type { FC } from "react";
+import { useNavigate } from "react-router";
 
-export const NotFoundSection: FC = () => (
-	<section className="h-screen flex flex-col items-center">
-		<div className="mt-[19vh]">
-			<img
-				src="/images/logo-dark.svg"
-				className="w-73 h-25"
-				alt="pycon 25 logo"
-			></img>
-		</div>
+export const NotFoundSection: FC = () => {
+  const navigate = useNavigate();
+  return (
+    <section className="h-screen flex flex-col items-center">
+      <div className="mt-[19vh]">
+        <img
+          src="/images/logo-dark.svg"
+          className="w-73 h-25"
+          alt="pycon 25 logo"
+        ></img>
+      </div>
 
-		<div className="font-sans text-[#282828] text-md mt-[46vh] text-center">
-			You are going to be redirected back to the homepage.
-		</div>
-	</section>
-);
+      <div className="mt-[46vh]">
+        <button
+          onClick={() => {
+            navigate("/");
+          }}
+          type="button"
+          className="cursor-pointer bg-[#224083] w-77 h-16 font-sans rounded-xl text-[#FAFAFA] text-2xl"
+        >
+          Back to Homepage
+        </button>
+      </div>
+    </section>
+  );
+};
