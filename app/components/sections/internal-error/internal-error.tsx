c9bb41644b1e4a58b02d0fd70c60362255c2ad4e
Adi Primanda Ginting <adi@adipginting.com>
2025-09-16 06:19:58 +0700

    fix: biome check

--- app/components/sections/internal-error/internal-error.tsx
+++ app/components/sections/internal-error/internal-error.tsx
@@ -1,17 +1,17 @@
 import type { FC } from "react";
 
 export const InternalErrorSection: FC = () => (
-  <section className="h-screen flex flex-col items-center">
-    <div className="mt-[19vh]">
-      <img
-        src="/images/logo-dark.svg"
-        className="w-73 h-25"
-        alt="pycon 25 logo"
-      ></img>
-    </div>
+	<section className="h-screen flex flex-col items-center">
+		<div className="mt-[19vh]">
+			<img
+				src="/images/logo-dark.svg"
+				className="w-73 h-25"
+				alt="pycon 25 logo"
+			></img>
+		</div>
 
-    <div className="font-sans text-[#282828] text-md mt-[46vh] text-center">
-      You are going to be redirected back to the homepage.
-    </div>
-  </section>
+		<div className="font-sans text-[#282828] text-md mt-[46vh] text-center">
+			You are going to be redirected back to the homepage.
+		</div>
+	</section>
 );
