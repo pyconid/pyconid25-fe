04927e1cf3ff05ae86e10b19ffaaee81982fd6cf
fikriwado <mfikri.khoirurrizal@gmail.com>
2025-10-03 16:53:30 +0700

    add base general color and setup not found page

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,8 @@
 import { index, type RouteConfig, route } from "@react-router/dev/routes";
 
 export default [
-	index("routes/home.tsx"),
-	route("/login", "routes/login.tsx"),
-	route("/register", "routes/register.tsx"),
+  index("routes/home.tsx"),
+  route("/login", "routes/login.tsx"),
+  route("/register", "routes/register.tsx"),
+  route("*", "routes/errors/404.tsx"),
 ] satisfies RouteConfig;
