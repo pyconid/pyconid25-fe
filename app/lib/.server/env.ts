c937c96864308c6960b853fc0d48106f88e8eef1
euxzy <mhd.rijal203@gmail.com>
2025-10-13 10:18:44 +0700

    chore(env): remove znv because of the version doesn't updated for zod v4

--- app/lib/.server/env.ts
+++ app/lib/.server/env.ts
@@ -1,11 +1,18 @@
 // This file is server-side only. Do not use it directly in the client as it will throw an error.
 
 import dotenv from "dotenv";
-import { parseEnv, z } from "znv/compat";
+import z from "zod";
 
 dotenv.config();
 
-export const parsedEnv = parseEnv(process.env, {
+function parseEnv<T>(env: NodeJS.ProcessEnv, schema: z.ZodObject) {
+	const parsed = schema.safeParse(env);
+	if (!parsed.success) throw new Error(parsed.error.message);
+	return parsed.data as T;
+}
+
+type EnvType = z.infer<typeof envSchema>;
+const envSchema = z.object({
 	NODE_ENV: z
 		.enum(["development", "staging", "production"])
 		.optional()
@@ -14,6 +21,8 @@ export const parsedEnv = parseEnv(process.env, {
 	SESSION_SECRET: z.string().min(1),
 	SESSION_PUBLIC_SECRET: z.string().min(1),
 });
+
+export const parsedEnv = parseEnv<EnvType>(process.env, envSchema);
 
 export const isDevelopment = parsedEnv.NODE_ENV === "development";
 export const isStaging = parsedEnv.NODE_ENV === "staging";
