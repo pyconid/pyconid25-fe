a3b7f4152058c23a8fe54c4bcb1f2ab544ce36b5
Adi Primanda Ginting <adi@adipginting.com>
2025-10-26 12:12:45 +0700

    feat: some part of the footer

--- app/components/layouts/navigation/footer.tsx
+++ app/components/layouts/navigation/footer.tsx
@@ -1,9 +1,8 @@
-import { Accordion } from "~/components/shared/accordion";
+import { Mail } from "lucide-react";
 import {
   OurTeamCard,
   type OurTeamCardProps,
 } from "~/components/shared/card/our-team";
-import { cn } from "~/lib/utils";
 
 const OURTEAM_CARDS: (OurTeamCardProps & { id: string })[] = Array.from({
   length: 10,
@@ -14,6 +13,9 @@ const OURTEAM_CARDS: (OurTeamCardProps & { id: string })[] = Array.from({
   description:
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged...",
 }));
+
+const OTHER = ["Everyone who can, pay", "Be A Sponsor", "Check Schedule"];
+const ABOUT = ["Terms of Service", "Privacy Policy", "Code of Conduct"];
 
 export const Footer = () => {
   return (
@@ -54,6 +56,72 @@ export const Footer = () => {
             {OURTEAM_CARDS.map(({ id, ...props }) => (
               <OurTeamCard key={id} {...props} />
             ))}
+          </div>
+        </div>
+
+        <div className="flex justify-between border rounded-xl bg-[#224083] h-[270px] px-20 pt-10 pb-7 gap-7 items-start  ">
+          <div className="flex flex-col gap-y-4">
+            <img
+              src="/images/logo-light.webp"
+              alt="logo light pycon"
+              className="w-40"
+            />
+            <div className="flex flex-col items-center gap-y-2">
+              <div className="font-sans text-white font-light">
+                Organized by:
+              </div>
+              <img
+                src="/images/logo-python-id.png"
+                alt="logo light pycon"
+                className="w-40"
+              />
+            </div>
+          </div>
+
+          <div className="w-70 flex flex-col gap-y-2">
+            <p className="font-display font-bold text-white text-2xl">
+              Contact Us
+            </p>
+            <div className="flex flex-col gap-y-4 ">
+              <div className="flex gap-x-2">
+                <Mail className="text-white" />
+                <p className="text-white">
+                  <a href="mailto:pycon@python.or.id">pycon@python.or.id</a>
+                </p>
+              </div>
+              <p className="text-white">
+                PyCon ID 2025 is organized by volunteers, so it may take more
+                time for us to reply to inquiries. Thank you for your patience.
+              </p>
+            </div>
+          </div>
+
+          <div className="w-70 flex flex-col gap-y-2">
+            <p className="font-display font-bold text-white text-2xl">Other</p>
+            <ul className="flex flex-col gap-y-2">
+              {OTHER.map((item) => (
+                <li key={item} className="font-sans text-white">
+                  <a href="https://" target="_blank" rel="noopener noreferer">
+                    {" "}
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="w-70 flex flex-col gap-y-2">
+            <p className="font-display font-bold text-white text-2xl">About</p>
+            <ul className="flex flex-col gap-y-2">
+              {ABOUT.map((item) => (
+                <li key={item} className="font-sans text-white">
+                  <a href="https://" target="_blank" rel="noopener noreferer">
+                    {" "}
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
