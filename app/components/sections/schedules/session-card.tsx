401240aa40aeb651e85fb60819b5da3eb2639248
aldimhr <dirikuin16@gmail.com>
2025-11-29 14:50:42 +0700

    add: component session-card

---
+++ app/components/sections/schedules/session-card.tsx
@@ -0,0 +1,32 @@
+interface SessionCardProps {
+  onClick: () => void
+}
+
+export const SessionCard = ({ onClick }: SessionCardProps) => {
+  return (
+    <div onClick={onClick}>
+      <div className="flex justify-between items-center">
+        <div className="text-xs">
+          <span>Podium #1</span>
+          <span className="text-[#F37F20]"> | </span>
+          <span className="font-bold">Keynote</span>
+        </div>
+        <div className="bg-[#D9D9D9] rounded-lg px-4 py-2 text-xs font-bold">
+          EN
+        </div>
+      </div>
+      <div className="bg-[#F1F1F1] border border-[#2B2B2B40]/25 rounded-lg mt-2 p-4 space-y-2">
+        <p className="font-bold text-xl">Meet / Network with people here</p>
+        <div className="flex items-center gap-2">
+          <div className="h-8 w-8 rounded-full">
+            <img
+              src="https://avatar.iran.liara.run/public/10"
+              alt="avatar"
+            />
+          </div>
+          <p className="text-sm">Livia Torff</p>
+        </div>
+      </div>
+    </div>
+  )
+}
