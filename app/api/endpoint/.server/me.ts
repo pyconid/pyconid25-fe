784c38b0999547323be52a81a4ccc2f6c7c89064
Adi Primanda Ginting <adi@adipginting.com>
2025-12-02 22:14:57 +0700

    fix: remove console log

--- app/api/endpoint/.server/me.ts
+++ app/api/endpoint/.server/me.ts
@@ -1,7 +1,5 @@
 import { http } from "~/lib/http/$.server";
 
 export const getMe = async ({ request }: { request: Request }) => {
-	console.log("BRO ", await http.get("/auth/me/", { request }));
-
 	return await http.get("/auth/me/", { request });
 };
