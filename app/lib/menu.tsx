41adcf0b5144eb4a67c812d920a39b0577d5d0f4
Dima M Dinama <dmaharika@gmail.com>
2025-12-08 04:08:58 +0000

    add redirect schedule and speaker

--- app/lib/menu.tsx
+++ app/lib/menu.tsx
@@ -3,7 +3,7 @@ const MENU = [
 	// { name: "About Us", href: "/about" },
 	{ name: "Ticket", href: "/ticket" },
 	// { name: "Proposals", href: "/call-for-proposal" },
-	{ name: "Schedule", href: "/schedules" },
+	{ name: "Schedules", href: "/schedules" },
 	{ name: "Speakers", href: "/speakers" },
 	{ name: "Sponsorship", href: "/call-for-sponsor" },
 	{ name: "Code of Conduct", href: "/code-of-conduct" },
